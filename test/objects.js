13cb499772542101be76b2defecd03cfa83cd400
ajlopez <webmaster@ajlopez.com>
2019-01-13 15:18:42 -0300

    Value object

--- test/objects.js
+++ test/objects.js
@@ -17,3 +17,11 @@ exports['put and get value'] = function (test) {
     test.equal(object.get(0), 42);
 };
 
+exports['value object'] = function (test) {
+    var value = objects.value(42);
+    
+    test.ok(value);
+    test.equal(typeof value, 'object');
+    test.equal(value.value(), 42);
+};
+
