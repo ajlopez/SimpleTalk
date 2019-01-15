e7c4d42464f5ea542382b0c6fc0c48a637ac8d57
ajlopez <webmaster@ajlopez.com>
2019-01-15 15:20:45 -0300

    Machine first opcodes

--- test/machines.js
+++ test/machines.js
@@ -1,5 +1,7 @@
 
 const machines = require('../lib/machines');
+const OpCodes = machines.OpCodes;
+const objects = require('../lib/objects');
 
 exports['create machine'] = function (test) {
     const machine = machines.machine();
@@ -7,3 +9,20 @@ exports['create machine'] = function (test) {
     test.ok(machine);
     test.equal(typeof machine, 'object');
 };
+
+exports['add two values'] = function (test) {
+    var one = objects.value(1);
+    var two = objects.value(2);
+    
+    const machine = machines.machine([ one, two ]);
+    
+    machine.execute([ OpCodes.LoadValue, 0, OpCodes.LoadValue, 1, OpCodes.Add ]);
+    
+    var stack = machine.stack();
+    
+    test.ok(stack);
+    test.ok(Array.isArray(stack));
+    test.equal(stack.length, 1);
+    test.equal(stack[0].value(), 3);
+};
+
