3ce9061d58981efcafe38bea600c7671aeeffadc
ajlopez <webmaster@ajlopez.com>
2019-01-16 14:45:53 -0300

    Store instance variable

--- test/machines.js
+++ test/machines.js
@@ -72,3 +72,21 @@ exports['load instance variable'] = function (test) {
     test.strictEqual(stack[0].value(), 42);
 };
 
+exports['store instance variable'] = function (test) {
+    var one = objects.value(1);
+    var two = objects.value(2);
+    
+    const obj = objects.object();
+    
+    const machine = machines.machine([ one, two ], obj);
+    
+    machine.execute([ OpCodes.LoadValue, 1, OpCodes.StoreVariable, 0 ]);
+    
+    var stack = machine.stack();
+    
+    test.ok(stack);
+    test.ok(Array.isArray(stack));
+    test.equal(stack.length, 0);
+    test.strictEqual(obj.get(0).value(), 2);
+};
+
