0edec7d93fe27cc42cdbdf190df2e87111b1b88e
ajlopez <webmaster@ajlopez.com>
2019-02-03 09:14:26 -0300

    Compile integer constant

--- test/compilers.js
+++ test/compilers.js
@@ -7,3 +7,17 @@ exports['create compiler as object'] = function (test) {
     test.ok(compiler);
     test.equal(typeof compiler, 'object');
 };
+
+exports['compile integer constant'] = function (test) {
+    const compiler = compilers.compiler();
+    
+    compiler.compileConstant(42);
+    
+    const values = compiler.values();
+    
+    test.ok(values);
+    test.ok(Array.isArray(values));
+    test.equal(values.length, 1);
+    test.strictEqual(values[0], 42);
+};
+
