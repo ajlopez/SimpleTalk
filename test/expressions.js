b134cbbe3309002d769934a493b34f6f592e399f
ajlopez <webmaster@ajlopez.com>
2019-02-21 16:44:27 -0300

    Create and compile send expression

--- test/expressions.js
+++ test/expressions.js
@@ -1,6 +1,7 @@
 
-var x = require('../lib/expressions');
-var compilers = require('../lib/compilers');
+const x = require('../lib/expressions');
+const compilers = require('../lib/compilers');
+const OpCodes = require('../lib/machines').OpCodes;
 
 exports['create and compile constant'] = function (test) {
     const expr = x.constant(42);
@@ -18,5 +19,34 @@ exports['create and compile constant'] = function (test) {
     test.ok(Array.isArray(values));
     test.equal(values.length, 1);
     test.strictEqual(values[0], 42);    
+};
+
+exports['create and compile send'] = function (test) {
+    const expr = x.send(x.constant(40), '+', [ x.constant(2) ]);
+
+    test.ok(expr);
+    test.equal(typeof expr, 'object');
+    
+    const compiler = compilers.compiler();
+    
+    expr.compile(compiler);
+    
+    const values = compiler.values();
+    
+    test.ok(values);
+    test.ok(Array.isArray(values));
+    test.equal(values.length, 2);
+    test.strictEqual(values[0], 40);
+    test.strictEqual(values[1], 2);
+
+    const bytecodes = compiler.bytecodes();
+
+    test.ok(bytecodes);
+    test.equal(bytecodes.length, 5);
+    test.equal(bytecodes[0], OpCodes.LoadValue);
+    test.equal(bytecodes[1], 0);
+    test.equal(bytecodes[2], OpCodes.LoadValue);
+    test.equal(bytecodes[3], 1);
+    test.equal(bytecodes[4], OpCodes.Add);
 };
 
