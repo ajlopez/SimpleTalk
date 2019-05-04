e010093096f4d4715fbcb72f3a710e6b3742ca2d
ajlopez <webmaster@ajlopez.com>
2019-05-04 15:01:07 -0300

    Parse real constant

--- test/parser.js
+++ test/parser.js
@@ -10,6 +10,13 @@ exports['parse integer constant'] = function (test) {
     test.deepEqual(geast.toObject(result), { ntype: 'constant', value: 42 });
 };
 
+exports['parse real constant'] = function (test) {
+    const result = parser.parse('real', '3.14159');
+    
+    test.ok(result);
+    test.deepEqual(geast.toObject(result), { ntype: 'constant', value: 3.14159 });
+};
+
 exports['parse string constant'] = function (test) {
     const result = parser.parse('string', "'foo'");
     
