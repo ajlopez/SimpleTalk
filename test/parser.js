0f7226cc7b959ddc529c0f7ce7c43acd90a7aad5
ajlopez <webmaster@ajlopez.com>
2019-05-04 14:59:29 -0300

    Parse string constant

--- test/parser.js
+++ test/parser.js
@@ -3,10 +3,17 @@ const parser = require('../lib/parser');
 
 const geast = require('geast');
 
-exports['parse constant'] = function (test) {
+exports['parse integer constant'] = function (test) {
     const result = parser.parse('integer', '42');
     
     test.ok(result);
     test.deepEqual(geast.toObject(result), { ntype: 'constant', value: 42 });
+};
+
+exports['parse string constant'] = function (test) {
+    const result = parser.parse('string', "'foo'");
+    
+    test.ok(result);
+    test.deepEqual(geast.toObject(result), { ntype: 'constant', value: 'foo' });
 };
 
